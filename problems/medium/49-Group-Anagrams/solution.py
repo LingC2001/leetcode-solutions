406e701316c133202e6e415396dcfd5c4ec5a146
myan0051 <myan0051@student.monash.edu>
2025-06-24 23:50:57 +1000

    feat: added python solution for 238. Product of Array Except Self

--- problems/medium/49-Group-Anagrams/solution.py
+++ problems/medium/49-Group-Anagrams/solution.py
@@ -1,11 +1,12 @@
 from typing import List
 
+
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
     seen_dict = {}
     res = []
     res_index = 0
     for string in strs:
-        signature = [0]*26
+        signature = [0] * 26
         for letter in string:
             signature[(ord(letter) - 97)] += 1
 
@@ -13,7 +14,7 @@ def groupAnagrams(strs: List[str]) -> List[List[str]]:
 
         if hashable_signature in seen_dict:
             res[seen_dict[hashable_signature]].append(string)
-            
+
         else:
             seen_dict[hashable_signature] = res_index
             res_index += 1
