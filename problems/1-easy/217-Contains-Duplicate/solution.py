9018954cc3486409d59fb16906f835837e71ed72
Ling Chen <lche0147@student.monash.edu>
2025-06-27 23:46:00 +1000

    fix: return bug in code

--- problems/1-easy/217-Contains-Duplicate/solution.py
+++ problems/1-easy/217-Contains-Duplicate/solution.py
@@ -2,9 +2,8 @@ def containsDuplicate(nums) -> bool:
     nums_set = set()
 
     for num in nums:
-
         if num in nums_set:
             return True
         else:
             nums_set.add(num)
-        return False
+    return False
