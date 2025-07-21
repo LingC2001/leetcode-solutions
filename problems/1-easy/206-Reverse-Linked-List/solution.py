d9b64a17a771bc44509e72384b1d38a2ddae8ee5
zhenlin zhang <zhenlinzhang15@gmail.com>
2025-07-21 20:59:42 +1000

    feat: added python solution for #206

---
+++ problems/1-easy/206-Reverse-Linked-List/solution.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    return prev
+
