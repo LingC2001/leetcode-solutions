27591e1fa95de8111f85f6f7c689245cb2a7e2b7
myan0051 <myan0051@student.monash.edu>
2025-08-08 21:52:38 +1000

    feat: added python code for #226

---
+++ problems/1-easy/226-Invert-Binary-Tree/solution.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            self.dfs(root)
+        return root
+       
+
+    def dfs(self, current):
+
+        if current.left:
+            self.dfs(current.left)
+
+        if current.right:
+            self.dfs(current.right)
+
+        current.left,current.right = current.right,current.left
