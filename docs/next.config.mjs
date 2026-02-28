00dbd3184f4d5931dfd4d1ec0e543660eb274227
Ling Chen <LingC2001@gmail.com>
2026-03-01 04:36:56 +1100

    fix: use NODE_ENV

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,14 +1,15 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
-const basePath = process.env.GITHUB_PAGES ? '/leetcode-solutions' : '';
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/leetcode-solutions' : '';
 
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
   reactStrictMode: true,
 
-  basePath,
+  basePath: basePath,
   assetPrefix: basePath + '/',
 
   serverExternalPackages: ['@takumi-rs/image-response'],
