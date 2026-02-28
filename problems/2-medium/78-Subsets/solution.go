114e2ec07429610a793fb398c65748070b87956f
Ling Chen <LingC2001@gmail.com>
2026-03-01 00:17:33 +1100

    chore: update golang code for 78

--- problems/2-medium/78-Subsets/solution.go
+++ problems/2-medium/78-Subsets/solution.go
@@ -1,19 +1,17 @@
 func subsets(nums []int) [][]int {
-    res := [][]int{{}}
+    res := [][]int{}
     path := []int{}
-    for i, _ := range nums {
-        dfs(nums, i, &path, &res)
-    }
+    dfs(nums, 0, &path, &res)
     return res
 }
 
-func dfs(nums []int, index int, path *[]int, res *[][]int) {
-    *path = append(*path, nums[index])
+func dfs(nums []int, start int, path *[]int, res *[][]int) {
     copy := append([]int{}, *path...)
     *res = append(*res, copy)
 
-    for i := index + 1; i < len(nums); i++ {
-        dfs(nums, i, path, res)
+    for i := start; i < len(nums); i++ {
+        *path = append(*path, nums[i])
+        dfs(nums, i+1, path, res)
+        *path = (*path)[:len(*path)-1]
     }
-    *path = (*path)[:len(*path)-1]
 }
